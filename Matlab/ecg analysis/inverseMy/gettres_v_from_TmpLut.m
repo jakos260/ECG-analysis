5b72a1df8fd485317ce5e89aeba28812e31c19ac
Jan Kost <jankost21@gmail.com>
2026-05-15 17:13:05 +0200

    add working version of inverseECG based on TT2 model

--- Matlab/ecg analysis/inverseMy/gettres_v_from_TmpLut.m
+++ Matlab/ecg analysis/inverseMy/gettres_v_from_TmpLut.m
@@ -3,61 +3,33 @@ function TST = gettres_v_from_TmpLut(INV, opt, keepopt, notchopt, amplopt)
     
     % 1. Rozróżnienie czasów (dep i rep)
     if strcmp(opt.type,'dep')
-        tims_dep = opt.tims; 
-        tims_rep = keepopt.tims;
+        tims_1 = opt.tims; 
+        tims_2 = keepopt.tims;
     else
-        tims_dep = keepopt.tims; 
-        tims_rep = opt.tims;
+        tims_1 = keepopt.tims; 
+        tims_2 = opt.tims;
     end
     
-    num_nodes = size(INV.T, 1);
-    num_samples = size(INV.T, 2);
-    TST.S = zeros(num_nodes, num_samples);
-    t_eval = INV.T(1, :); % wektor czasu próbkowania EKG
-    
-    % Wymagane: INV.LUT (Twój pre-kalkulowany słownik)
-    % Wymagane: INV.RegionIdx (Wektor 1500x1, przypisujący węzeł do ID klastra, np. 1-50)
-    
-    num_regions = max(INV.RegionIdx);
-    
-    % 2. APLIKACJA SŁOWNIKA DO KLASTRÓW
-    for r = 1:num_regions
-        % Znajdź wszystkie węzły należące do tego klastra
-        nodes_in_region = find(INV.RegionIdx == r);
-        
-        % Oblicz średnie oczekiwane APD dla tego obszaru wg optymalizatora
-        avg_dep = mean(tims_dep(nodes_in_region));
-        avg_rep = mean(tims_rep(nodes_in_region));
-        target_APD = avg_rep - avg_dep;
-        
-        % Znajdź w słowniku LUT przebieg o fizjologicznym APD najbliższym target_APD
-        all_apds = [INV.LUT.APD];
-        [~, best_lut_idx] = min(abs(all_apds - target_APD));
-        
-        % Pobieramy idealny, fizjologiczny kształt fazy 3
-        V_template = INV.LUT(best_lut_idx).V;
-        APD_template = INV.LUT(best_lut_idx).APD; % Czas trwania tego wzorca
-        
-        % Aplikujemy ten wzorzec do wszystkich węzłów w klastrze,
-        % przesuwając go w czasie zgodnie z ICH WŁASNYM czasem depolaryzacji
-        for n = 1:length(nodes_in_region)
-            node = nodes_in_region(n);
-            dep_n = tims_dep(node);
-            
-            % Mapowanie tylko w oparciu o przesunięcie (brak rozciągania fazy!)
-            % Ponieważ V_template ma już wymaganą długość (wybraną ze słownika)
-            t_mapped = t_eval - dep_n + (APD_template / 2); % Uproszczone wyrównanie
-            
-            % Tu wykorzystujemy Twoje dt=0.1s (wzorce ze słownika) do interpolacji na czas EKG
-            t_tt2 = (0:length(V_template)-1) * 0.1; % czas oryginalnego generatora
-            
-            % Wpisanie do dużej macierzy 
-            TST.S(node, :) = interp1(t_tt2 + dep_n, V_template, t_eval, 'linear', 0);
-        end
-    end
+    % 2. Generowanie potencjałów przy użyciu naszego słownika LUT i regionów AHA
+    TST.S = my_regional_Smode(INV, tims_1, tims_2);
 
     % 3. ORYGINALNA LOGIKA (BŁĘDY I REGULARYZACJA)
-    % (Dokładnie taka sama reszta jak w poprzedniej odpowiedzi...)
-    TST.PHIA = lowpassma(INV.AMA * TST.S, INV.lpass); 
-    % ... i tak dalej
+    TST.PHIA    = lowpassma(INV.AMA * TST.S, INV.lpass); 
+    TST.RES     = INV.PHIREF - TST.PHIA(1:size(INV.PHIREF,1), 1:size(INV.PHIREF,2));
+    TST.rd      = norm(TST.RES,'fro') / INV.normphi;
+    TST.wrd     = sum(rms(TST.RES) ./ (wrd_param + rms(INV.PHIREF)));
+    
+    % Determine regularization value:
+    if strcmp(opt.type,'dep')
+        TST.reg = norm(INV.REGOP * opt.tims);
+    else
+        TST.reg = norm(INV.REGOPREP * opt.tims);
+    end
+    
+    % Determine value of regularization term plus RD value:
+    if INV.useWeighedRd
+        TST.tres = sqrt(TST.wrd^2 + (TST.reg * opt.mu)^2);
+    else
+        TST.tres = sqrt(TST.rd^2 + (TST.reg * opt.mu)^2);
+    end
 end
--- Matlab/ecg analysis/inverseMy/my_inversefunc.m
+++ Matlab/ecg analysis/inverseMy/my_inversefunc.m
@@ -1,13 +1,5 @@
 function meas = my_inversefunc(GEOM, initdep, initrep, mudep, murep)
 % MY_INVERSEFUNC - Simplified inverse problem for depolarization and repolarization
-% 
-% INPUTS:
-%   GEOM    - Wcześniej przygotowana struktura geometrii i sygnału (z loadera)
-%   initdep - Ogniska/czasy początkowe dla depolaryzacji (np. z multifociscan)
-%   initrep - Ogniska/czasy początkowe dla repolaryzacji
-%   mudep   - Parametr regularyzacji (Laplasjan) dla depolaryzacji (np. 5e-6)
-%   murep   - Parametr regularyzacji (Laplasjan) dla repolaryzacji (np. 1e-5)
-
 disp('Inicjalizacja uproszczonego inversefunc...');
 
 %% PARAMETRY GŁÓWNE
@@ -17,17 +9,17 @@ INV.lambopt = 0.001;
 INV.stopcrit = 2e-4;
 INV.lambdamax = 500;
 INV.MINRD = 0.15;
-INV.mode = 4; % Mode 4 oznacza optymalizację Depolaryzacji i Repolaryzacji
-INV.lpass = 1; % Zmiana z 5 na 1 (mniejsze wygładzanie, opcjonalnie)
+INV.mode = 4;
+INV.lpass = 1;
 INV.useWeighedRd = 0;
 
-% Domyślne wartości z GEOM, bo ich potrzebujemy
 INV.SPECS = GEOM.SPECS;
 INV.AMA = GEOM.AMA;
 INV.ATA = INV.AMA' * INV.AMA;
+INV.RegionIdx = GEOM.RegionIdx;
+INV.LUT = GEOM.LUT;
 
 %% PRZYGOTOWANIE SYGNAŁU REFERENCYJNEGO
-% Baseline correction and low-pass filtering
 INV.BSM = baselinecor(lowpassma(GEOM.BSM(:, GEOM.SPECS.onsetqrs:GEOM.SPECS.endtwave), INV.lpass));
 INV.PHIREF = INV.BSM;
 INV.normphi = norm(INV.PHIREF, 'fro');
@@ -35,34 +27,20 @@ INV.normphi = norm(INV.PHIREF, 'fro');
 usetimes = size(INV.PHIREF, 2);
 INV.T = ones(size(GEOM.AMA, 2), 1) * (0 : usetimes - 1);
 
-%% REGULARYZACJA (Laplasjan na powierzchni siatki trójkątnej)
-[INV.REGOP, INV.REGOPREP] = calcREGOP(GEOM, 1); % Używamy domyślnego surflapl
+%% REGULARYZACJA
+[INV.REGOP, INV.REGOPREP] = calcREGOP(GEOM, 1);
 INV.ROTRO = INV.REGOP' * INV.REGOP;
 INV.ROTROREP = INV.REGOPREP' * INV.REGOPREP;
 
 %% INICJALIZACJA STRUKTUR OPTYMALIZACYJNYCH
 OPT = struct();
-
-% Struktura dla depolaryzacji
-OPT.DEP.mu = mudep;
-OPT.DEP.lambopt = INV.lambopt;
-OPT.DEP.tims = initdep;
-OPT.DEP.type = 'dep';
-
-% Struktura dla repolaryzacji
-OPT.REP.mu = murep;
-OPT.REP.lambopt = INV.lambopt;
-OPT.REP.tims = initrep;
-OPT.REP.type = 'apd'; % 'apd' oznacza, że optymalizujemy odległość między dep a rep
-
-% Puste struktury dla wyłączonych modułów (wymagane w argumentach funkcji pomocniczych)
-OPT.NOT.pot = zeros(size(GEOM.VER,1),1); % Notch - wyłączony
-OPT.AMP.pot = ones(size(GEOM.VER,1),1);  % Amplituda - wyłączona i ustawiona na 1
+OPT.DEP.mu = mudep; OPT.DEP.lambopt = INV.lambopt; OPT.DEP.tims = initdep; OPT.DEP.type = 'dep';
+OPT.REP.mu = murep; OPT.REP.lambopt = INV.lambopt; OPT.REP.tims = initrep; OPT.REP.type = 'apd';
+OPT.NOT.pot = zeros(size(GEOM.VER,1),1);
+OPT.AMP.pot = ones(size(GEOM.VER,1),1);
 
 %% WSTĘPNA WERYFIKACJA STANU POCZĄTKOWEGO
-INV.LUT = getTmpLut(0.8, 1.2, 0.2);
-TST = gettres_v_from_TT2mod(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
-% TST = gettres_v_from_TT2mod(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
+TST = gettres_v_from_TmpLut(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
 
 disp('-------------------------------------------------------------------------');
 disp(' iter    minD    maxD   stdD   minR    maxR   stdR     reg      rd     tres');
@@ -71,16 +49,14 @@ fprintf(' %3d %7.1f %7.1f %6.1f %6.1f %7.1f %6.1f %7.1f %7.4f %7.4f\n', ...
     min(OPT.REP.tims), max(OPT.REP.tims), std(OPT.REP.tims), ...
     TST.reg, TST.rd, TST.tres);
 
-%% GŁÓWNA PĘTLA ITERACYJNA MARQUARDTA
-iter = 0;
-depscore = 1;
-repscore = 1;
+iter = 0; depscore = 1; repscore = 1;
 
+%% GŁÓWNA PĘTLA ITERACYJNA MARQUARDTA
 while iter < INV.maxiter && (depscore || repscore) && TST.rd > INV.MINRD
     iter = iter + 1;
     
     % Krok 1: Optymalizacja Depolaryzacji
-    [depscore, OPT.DEP, TST] = optimizeDepRep(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
+    [depscore, OPT.DEP, TST] = my_optimizeDepRep(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
     opt = OPT.DEP;
     
     fprintf('D%3d %7.1f %7.1f %6.1f %6.1f %7.1f %6.1f %7.1f %7.4f %7.4f\n', ...
@@ -88,8 +64,8 @@ while iter < INV.maxiter && (depscore || repscore) && TST.rd > INV.MINRD
         min(OPT.REP.tims), max(OPT.REP.tims), std(OPT.REP.tims), ...
         TST.reg, TST.rd, TST.tres);
 
-    % Krok 2: Optymalizacja Repolaryzacji (jeśli jest potrzebna)
-    [repscore, OPT.REP, TST] = optimizeDepRep(INV, OPT.REP, OPT.DEP, OPT.NOT, OPT.AMP);
+    % Krok 2: Optymalizacja Repolaryzacji 
+    [repscore, OPT.REP, TST] = my_optimizeDepRep(INV, OPT.REP, OPT.DEP, OPT.NOT, OPT.AMP);
     opt = OPT.REP;
 
     fprintf('R%3d %7.1f %7.1f %6.1f %6.1f %7.1f %6.1f %7.1f %7.4f %7.4f\n', ...
@@ -100,9 +76,8 @@ end
 
 disp('-------------------------------------------------------------------------');
 
-%% ZWRÓCENIE WYNIKÓW
 % Ostatnia kontrola parametrów
-TST = gettres_v_from_TT2mod(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
+TST = gettres_v_from_TmpLut(INV, OPT.DEP, OPT.REP, OPT.NOT, OPT.AMP);
 
 meas = struct();
 meas.depfinal = OPT.DEP.tims;
@@ -115,6 +90,4 @@ meas.corfinal = COR(2,1);
 meas.iterfinal = iter;
 
 disp(['Zakończono optymalizację po ', num2str(iter), ' iteracjach. RD = ', num2str(meas.rdfinal)]);
-
 end
-
